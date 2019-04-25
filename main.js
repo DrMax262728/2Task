b3ff6f4664a9f9e24abcf59a1763fcfe56748875
DrMax262728 <perfectworld3843@gmail.com>
2019-04-25 22:04:32 +0300

    v1.2 Update func2

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ function func1(arr, gender) {
     return arr.filter( obj => obj.gender === gender);
 }
 
-function func2(arr, obj) {
-    "use strict";
-    let newArr = [];
-    let [firstObj = "Empty", secondObj = "Empty", , ...other] = arr;
-    newArr.push(firstObj);
-    newArr.push(secondObj);
-    newArr.push(obj);
-    newArr = newArr.concat(other);
-    return newArr;
+function func2(arr, obj, index) {
+    return [...arr.slice(0, index), obj, ...arr.slice(index)];
 }
